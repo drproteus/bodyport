01769a68d4d8667d11f3f1fe5ff243cb1f688bb0
Jake Goritski <jgoritski@gmail.com>
2022-03-13 15:09:22 -0400

    dockerize and prepare to use more production ready services for db and files

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM python:3.8
+COPY requirements.txt requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+COPY . /app
+WORKDIR /app
--- bodyport/settings.py
+++ bodyport/settings.py
@@ -65,8 +65,12 @@ WSGI_APPLICATION = "bodyport.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "bodyport",
+        "USER": "bodyport",
+        "PASSWORD": "bodyport",
+        "HOST": "db",
+        "PORT": 5432
     }
 }
 
