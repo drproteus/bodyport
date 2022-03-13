ccf3e7926dc38f97e1b53fb25e41e5b8779710b3
Jake Goritski <jgoritski@gmail.com>
2022-03-13 17:36:59 -0400

    misc cleanup

--- ecgzip/views.py
+++ ecgzip/views.py
@@ -1,32 +1,9 @@
 from django.shortcuts import redirect, render, HttpResponse
 from django.urls import reverse
 from django.views import View
-from django.http import Http404, JsonResponse
-from django.core.files.base import ContentFile
-from util.compression import ECGCompressor
 from ecgzip.forms import CompressionForm
 from ecgzip.models import CompressedECG
 from uuid import uuid4
-
-from django.views.decorators.csrf import csrf_exempt  # DEBUG!!!
-
-
-@csrf_exempt
-def ecg_compress(request):
-    form = CompressionForm(request.POST, request.FILES)
-    print(request.POST)
-    if not form.is_valid():
-        print(form.errors)
-        return HttpResponse(status=500)
-    record = form.save()
-    return JsonResponse(
-        {
-            "size": record.size,
-            "original_size": record.original_size,
-            "compression_ratio": record.compression_ratio,
-            "link": record.data.url,
-        }
-    )
 
 
 class ECGCompress(View):
