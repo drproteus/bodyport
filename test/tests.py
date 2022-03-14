a7eca302c81a8329248e7ee14e75680387a7408d
Jake Goritski <jgoritski@gmail.com>
2022-03-14 10:59:01 -0400

    add construct structure test

--- test/tests.py
+++ test/tests.py
@@ -1,4 +1,5 @@
 from util.compression import ECGCompressor
+from util.formats import ECGData, ECGCompressed
 
 
 def test_decompression_restores_from_compression():
@@ -7,3 +8,21 @@ def test_decompression_restores_from_compression():
     compressed = ECGCompressor.compress(original)
     decompressed = ECGCompressor.decompress(compressed)
     assert original == decompressed
+
+
+def test_compression_structure():
+    test_sequence = [0, 1000, 340, 65534, 4570]
+    deltas = [0, 1000, 340, 65534, 4570]
+    small = [0, 1000, -660]
+    big = [65194, -60964]
+    locs = [3, 4]
+    test_data = ECGCompressed.parse(
+        ECGCompressor.compress(ECGData.build(test_sequence))
+    )
+    assert test_data["len_origin"] == len(deltas)
+    assert test_data["len_small"] == len(small)
+    assert test_data["len_big"] == len(big)
+    assert test_data["len_locs"] == len(locs)
+    assert test_data["small"] == small
+    assert test_data["big"] == big
+    assert test_data["locs"] == locs
